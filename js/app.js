f5f1b0e855a417c89684e9afbc5a68c26112b8d5
Wielton <grantwielgosz@gmail.com>
2022-03-21 23:53:25 -0600

    Tried adding some functions for creating and pushing objects dynamically.  Tried extracting userAge and filtering to a new array but can't quite get it.  I'll keep working on it and hopefully get the correct anser by tomorrow

--- js/app.js
+++ js/app.js
@@ -4,83 +4,162 @@
 
 var tweets = ['Tweet 1', 'Tweet 2', 'Tweet 3', 'Tweet 4', 'Tweet 5', 'Tweet 6', 'Tweet 7', 'Tweet 8', 'Tweet 9', 'Tweet 10'];
 
-// This is a forEach loop, using myTweets as the callback.  A tweet is a value or element in my array
-tweets.forEach(myTweets);
-function myTweets(tweet){
+
+//
+
+tweets.forEach(function(tweet) {
     console.log(tweet);
-}
+});
 
 
-// This is my preferred use of a forEach callback
-// Simple and very easy to read
 tweets.forEach(tweet => console.log(tweet));
 
 
 // 10 user objects I will iterate through to extract the tweet, username, and dateCreated
 
-var users = [{
-    tweet: 'Hello',
-    username: 'John',
-    dateCreated: 'MM-DD-YYYY',
-    userAge: 15},
-    {
-    tweet: 'Hello',
-    username: 'John',
-    dateCreated: 'MM-DD-YYYY',
-    userAge: 31
-    },
-    {
-    tweet: 'Hello',
-    username: 'John',
-    dateCreated: 'MM-DD-YYYY',
-    userAge: 11
-    },
-    {
-    tweet: 'Hello',
-    username: 'John',
-    dateCreated: 'MM-DD-YYYY',
-    userAge: 32
-    },
-    {
-    tweet: 'Hello',
-    username: 'John',
-    dateCreated: 'MM-DD-YYYY',
-    userAge: 25
-    },
-    {
-    tweet: 'Hello',
-    username: 'John',
-    dateCreated: 'MM-DD-YYYY',
-    userAge: 43
-    },
-    {
-    tweet: 'Hello',
-    username: 'John',
-    dateCreated: 'MM-DD-YYYY',
-    userAge: 21
-    },
-    {
-    tweet: 'Hello',
-    username: 'John',
-    dateCreated: 'MM-DD-YYYY',
-    userAge: 24
-    },
-    {
-    tweet: 'Hello',
-    username: 'John',
-    dateCreated: 'MM-DD-YYYY',
-    userAge: 54
-    },
-    {
-    tweet: 'Hello',
-    username: 'John',
-    dateCreated: 'MM-DD-YYYY',
-    userAge: 32
-    }];
 
-    //  I could only extract the tweet from each object, didn't have time to finish!!!!
+var users1 = [{
+            tweet: 'Hello',
+            username: 'Mike',
+            dateCreated: 'MM-DD-YYYY',
+            userAge: 15
+        },
+            {
+            tweet: 'Hello',
+            username: 'Alex',
+            dateCreated: 'MM-DD-YYYY',
+            userAge: 31
+            },
+            {
+            tweet: 'Hello',
+            username: 'Colin',
+            dateCreated: 'MM-DD-YYYY',
+            userAge: 11
+            },
+            {
+            tweet: 'Hello',
+            username: 'John',
+            dateCreated: 'MM-DD-YYYY',
+            userAge: 32
+            },
+            {
+            tweet: 'Hello',
+            username: 'John',
+            dateCreated: 'MM-DD-YYYY',
+            userAge: 25
+                },
+            {
+            tweet: 'Hello',
+            username: 'John',
+            dateCreated: 'MM-DD-YYYY',
+            userAge: 43
+            },
+            {
+            tweet: 'Hello',
+            username: 'John',
+            dateCreated: 'MM-DD-YYYY',
+            userAge: 21
+            },
+            {
+            tweet: 'Hello',
+            username: 'John',
+            dateCreated: 'MM-DD-YYYY',
+            userAge: 24
+            },
+            {
+            tweet: 'Hello',
+            username: 'John',
+            dateCreated: 'MM-DD-YYYY',
+            userAge: 54
+            },
+            {
+            tweet: 'Hello',
+            username: 'John',
+            dateCreated: 'MM-DD-YYYY',
+            userAge: 32
+            }];
+
+
+//  I was trying to create an addNewUser function to inject user objects into a users array:
+
+// const users = new Array(i);
+
+// function addNewUser(newUser) {
+//     const newUser = {};
+//     for (i = 0; i < users.length; i++) {
+//         users[i] = {
+//                 tweet: '',
+//                 username: '',
+//                 dateCreated: '',
+//                 userAge: ''
+//         } 
+//     } return newUser;
+// }
+
+// addNewUser('This isi', 'uhsdf', 'njafudifg', 'ijadfgj');
+// console.log(users);
+
+
+// function newUser(tweet, username, dateCreated, userAge) {
+//     this.tweet = tweet;
+//     this.username = username;
+//     this.dateCreated = dateCreated;
+//     this.userAge = userAge;
+//     this.addUser = function() {
+//         return (`${this.tweet} ${this.username} ${this.dateCreated} ${this.userAge}`);
+//     }
     
-    for (let i = 0; i < users.length; i++) { 
-        console.log(users[i].tweet);
-        
+//     } 
+
+
+
+// userObjects.push(new Person('Hey this was so cool!', 'John', 'Date()', 25));
+
+// console.log(userObjects);
+
+// let userObjects = new Object();
+// userObjects.tweet = "OMG, I'm learning how to do this";
+// userObjects.username = 'Roger';
+// userObjects.dateCreated = Date();
+// userObjects.userAge = 32;
+
+// console.log(userObjects);
+//  I could only extract the tweet from each object, didn't have time to finish!!!!
+
+users1.forEach(users1 => console.log(users1.username, users1.userAge));
+
+// function ageVerification(str) {
+//     for (i = 0; i < str.length; i++) {
+//         if (str[i].userAge >= 18) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     }
+// }
+
+// console.log(ageVerification(users1));
+
+// tweets.forEach(function(tweet) {
+//     console.log(tweet);
+// });
+
+
+function ageVerification(arr) {
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i].userAge < 18) {
+            return arr[i].username;
+        } else {
+            return false;
+        }
     }
+        
+    
+    
+    }
+
+
+
+
+console.log(ageVerification(users1));
+
